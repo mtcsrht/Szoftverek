ac514d6fd16aa211c62c6b740a9204ac8b5310ca
Mate <mate@localhost.hu>
2023-10-27 13:34:26 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+namespace Szoftverek
+{
+    internal class Program
+    {
+        static List<Szoftver> szoftverek = new();
+        static void Main()
+        {
+            using var sr = new StreamReader(path: @"..\..\..\src\szoftverek.txt", encoding: Encoding.UTF8);
+            _ = sr.ReadLine();
+            while (!sr.EndOfStream) szoftverek.Add(new Szoftver(sr.ReadLine()));
+
+            Console.WriteLine("6. feladat");
+            szoftverek.ForEach(sz => Console.WriteLine(sz));
+
+            Console.WriteLine("7. feladat");
+            var f7 = szoftverek.Where(s => s.Kategoria == "antivírus" && s.Ertekeles > 8.5);
+            Console.WriteLine($"{f7.Count()}db van belőle.");
+
+            var maxErtekeles = szoftverek.Max(s => s.Ertekeles) - .1;
+            var f8 = szoftverek.Where(s => s.Ertekeles < maxErtekeles);
+
+
+        }
+    }
+}
---
+++ Szoftver.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Szoftverek
+{
+    class Szoftver
+    {
+        public int Azonosito { get; init; }
+        public string NevVerzio { get; init; }
+        public string LicencTipus { get; init; }
+        public string OpRendszerek { get; init; }
+        public string Kategoria { get; init; }
+        public double Ertekeles { get; init; }
+        public double NettoAr { get; init; }
+        public int Adokukcs { get; init; }
+
+        public Szoftver(string sor)
+        {
+            string[] s = sor.Split('|');
+
+            Azonosito = int.Parse(s[0]);
+            NevVerzio = s[1];
+            LicencTipus = s[2];
+            OpRendszerek = s[3];
+            Kategoria = s[4];
+            Ertekeles = double.Parse(s[5]);
+            NettoAr = double.Parse(s[6]);
+            Adokukcs = int.Parse(s[7]);
+
+        }
+
+        public override string ToString()
+        {
+            return $"Azonosító: {Azonosito}\n" +
+                $"Név és verziószám: {NevVerzio}\n" +
+                $"Licenc típus: {LicencTipus}\n" +
+                $"Operációs rendszer(ek): {OpRendszerek}\n" +
+                $"Szoftver kategóriája: {Kategoria}\n" +
+                $"Felhasználói értékelés: {Ertekeles}\n" +
+                $"Nettó ár: {NettoAr} USD\n" +
+                $"Adókulcs: {Adokukcs}\n";
+        }
+
+
+    }
+}
